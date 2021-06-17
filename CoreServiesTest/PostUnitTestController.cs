35b40698ab1f970c3f83711adfe87255cca980b4
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-06-17 16:28:07 +0530

    updated

--- CoreServiesTest/PostUnitTestController.cs
+++ CoreServiesTest/PostUnitTestController.cs
@@ -144,7 +144,180 @@ namespace CoreServiesTest
         }
 
         #endregion
+        #region Get All  
 
+        [Fact]
+        public async void Task_GetPosts_Return_OkResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+
+            //Act  
+            var data = await controller.GetPosts();
+
+            //Assert  
+            Assert.IsType<OkObjectResult>(data);
+        }
+
+        [Fact]
+        public void Task_GetPosts_Return_BadRequestResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+
+            //Act  
+            var data = controller.GetPosts();
+            data = null;
+
+            if (data != null)
+                //Assert  
+                Assert.IsType<BadRequestResult>(data);
+        }
+
+        [Fact]
+        public async void Task_GetPosts_MatchResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+
+            //Act  
+            var data = await controller.GetPosts();
+
+            //Assert  
+            Assert.IsType<OkObjectResult>(data);
+
+            var okResult = data.Should().BeOfType<OkObjectResult>().Subject;
+            var post = okResult.Value.Should().BeAssignableTo<List<PostViewModel>>().Subject;
+
+            Assert.Equal("Test Title 1", post[0].Title);
+            Assert.Equal("Test Description 1", post[0].Description);
+
+            Assert.Equal("Test Title 2", post[1].Title);
+            Assert.Equal("Test Description 2", post[1].Description);
+        }
+
+        #endregion
+        #region Update Existing Blog  
+
+        [Fact]
+        public async void Task_Update_ValidData_Return_OkResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            var postId = 2;
+
+            //Act  
+            var existingPost = await controller.GetPost(postId);
+            var okResult = existingPost.Should().BeOfType<OkObjectResult>().Subject;
+            var result = okResult.Value.Should().BeAssignableTo<PostViewModel>().Subject;
+
+            var post = new Post();
+            post.Title = "Test Title 2 Updated";
+            post.Description = result.Description;
+            post.CategoryId = result.CategoryId;
+            post.CreatedDate = result.CreatedDate;
+
+            var updatedData = await controller.UpdatePost(post);
+
+            //Assert  
+            Assert.IsType<OkResult>(updatedData);
+        }
+
+        [Fact]
+        public async void Task_Update_InvalidData_Return_BadRequest()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            var postId = 2;
+
+            //Act  
+            var existingPost = await controller.GetPost(postId);
+            var okResult = existingPost.Should().BeOfType<OkObjectResult>().Subject;
+            var result = okResult.Value.Should().BeAssignableTo<PostViewModel>().Subject;
+
+            var post = new Post();
+            post.Title = "Test Title More Than 20 Characteres";
+            post.Description = result.Description;
+            post.CategoryId = result.CategoryId;
+            post.CreatedDate = result.CreatedDate;
+
+            var data = await controller.UpdatePost(post);
+
+            //Assert  
+            Assert.IsType<BadRequestResult>(data);
+        }
+
+        [Fact]
+        public async void Task_Update_InvalidData_Return_NotFound()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            var postId = 2;
+
+            //Act  
+            var existingPost = await controller.GetPost(postId);
+            var okResult = existingPost.Should().BeOfType<OkObjectResult>().Subject;
+            var result = okResult.Value.Should().BeAssignableTo<PostViewModel>().Subject;
+
+            var post = new Post();
+            post.PostId = 5;
+            post.Title = "Test Title More Than 20 Characteres";
+            post.Description = result.Description;
+            post.CategoryId = result.CategoryId;
+            post.CreatedDate = result.CreatedDate;
+
+            var data = await controller.UpdatePost(post);
+
+            //Assert  
+            Assert.IsType<NotFoundResult>(data);
+        }
+
+        #endregion
+        #region Delete Post  
+
+        [Fact]
+        public async void Task_Delete_Post_Return_OkResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            var postId = 2;
+
+            //Act  
+            var data = await controller.DeletePost(postId);
+
+            //Assert  
+            Assert.IsType<OkResult>(data);
+        }
+
+        [Fact]
+        public async void Task_Delete_Post_Return_NotFoundResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            var postId = 5;
+
+            //Act  
+            var data = await controller.DeletePost(postId);
+
+            //Assert  
+            Assert.IsType<NotFoundResult>(data);
+        }
+
+        [Fact]
+        public async void Task_Delete_Return_BadRequestResult()
+        {
+            //Arrange  
+            var controller = new PostController(repository);
+            int? postId = null;
+
+            //Act  
+            var data = await controller.DeletePost(postId);
+
+            //Assert  
+            Assert.IsType<BadRequestResult>(data);
+        }
+
+        #endregion
     }
 
 
